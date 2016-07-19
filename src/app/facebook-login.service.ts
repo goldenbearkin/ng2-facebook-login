d06ca47cf6c49722e30531843031dd4e09c4837d
jasonhui <huihangkin@gmail.com>
2016-07-19 14:56:35 +0800

    bug fix

--- src/app/facebook-login.service.ts
+++ src/app/facebook-login.service.ts
@@ -13,9 +13,9 @@ import { Injectable } from '@angular/core';
 */
 
 declare var FB: any;
-// declare var AWS: any;
-declare let AWSCognito:any;
-declare var apigClientFactory: any;
+declare let AWS: any;
+// declare let AWSCognito:any;
+declare let apigClientFactory: any;
 
 export interface FbStatus {
   status: string;
@@ -46,7 +46,7 @@ export class FacebookLoginService {
 
       (<any>window).fbAsyncInit = () => {
         FB.init({
-          appId      : '1650752755250939',
+          appId      : '253815588337573',
           cookie     : true,  // enable cookies to allow the server to access the session 
           xfbml      : true,  // parse social plugins on this page
           version    : 'v2.2' // use version 2.2
@@ -60,76 +60,76 @@ export class FacebookLoginService {
   }
 
   login = () => {
-    // FB.login(response => {
-    // //   this.status$.next(response);
+    FB.login(response => {
+    //   this.status$.next(response);
       
 
-    //   if (response.authResponse) {
+      if (response.authResponse) {
 
-    //       console.log('You are now logged in.');
-    //       console.log(AWSCognito);
+          console.log('You are now logged in.');
+          console.log(AWS);
 
-    //       // Add the Facebook access token to the Cognito credentials login map.
-    //       AWSCognito.config.region = 'ap-northeast-1';
+          // Add the Facebook access token to the Cognito credentials login map.
+          AWS.config.region = 'ap-northeast-1';
 
-    //       AWSCognito.config.credentials = new AWSCognito.CognitoIdentityCredentials({
-    //         IdentityPoolId: 'ap-northeast-1:fb4a0ae9-c0ba-4a3c-b8f4-ca4c3c1ceab9',
-    //         Logins: {
-    //           'graph.facebook.com': response.authResponse.accessToken
-    //         }
-    //       });
+          AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+            IdentityPoolId: 'ap-northeast-1:88e9e508-dd0a-4d2f-adb7-587d60092e4c',
+            Logins: {
+              'graph.facebook.com': response.authResponse.accessToken
+            }
+          });
 
-    //       // Obtain AWS credentials
-    //       AWSCognito.config.credentials.get(err => {
-    //           if (err) {
-    //              alert("alert: " + err);
-    //              return;
-    //           }
-    //           console.log(AWSCognito.config.credentials.accessKeyId);
-    //           console.log(AWSCognito.config.credentials.secretAccessKey);
-    //           console.log(AWSCognito.config.credentials.sessionToken);
-    //           // this.response = "Cognito Identity Id", AWS.config.credentials.identityId 
-    //           // setTimeout(()=>{},0);
-    //           // alert("Cognito Identity Id", AWS.config.credentials.identityId);
+          // Obtain AWS credentials
+          AWS.config.credentials.get(err => {
+              if (err) {
+                 alert("alert: " + err);
+                 return;
+              }
+              console.log(AWS.config.credentials.accessKeyId);
+              console.log(AWS.config.credentials.secretAccessKey);
+              console.log(AWS.config.credentials.sessionToken);
+              // this.response = "Cognito Identity Id", AWS.config.credentials.identityId 
+              // setTimeout(()=>{},0);
+              // alert("Cognito Identity Id", AWS.config.credentials.identityId);
 
-    //           var apigClient = apigClientFactory.newClient({
-    //             defaultContentType: 'application/json',
-    //             defaultAcceptType: 'application/json',
-    //             accessKey: AWSCognito.config.credentials.accessKeyId,
-    //             secretKey: AWSCognito.config.credentials.secretAccessKey,
-    //             sessionToken: AWSCognito.config.credentials.sessionToken, //OPTIONAL: If you are using temporary credentials you must include the session token
-    //             region: 'ap-northeast-1' // OPTIONAL: The region where the API is deployed, by default this parameter is set to us-east-1
-    //           });
+              var apigClient = apigClientFactory.newClient({
+                defaultContentType: 'application/json',
+                defaultAcceptType: 'application/json',
+                accessKey: AWS.config.credentials.accessKeyId,
+                secretKey: AWS.config.credentials.secretAccessKey,
+                sessionToken: AWS.config.credentials.sessionToken, //OPTIONAL: If you are using temporary credentials you must include the session token
+                region: 'ap-northeast-1' // OPTIONAL: The region where the API is deployed, by default this parameter is set to us-east-1
+              });
 
 
               
-    //           var params = {
-    //             //This is where any header, path, or querystring request params go. The key is the parameter named as defined in the API
-    //               // 'Content-Type': 'application/json',
-    //               // 'headers': 'Content-Type: application/json',
-    //                headers: {
-    //                   'Content-Type': 'application/json'
-    //               },
+              var params = {
+                //This is where any header, path, or querystring request params go. The key is the parameter named as defined in the API
+                  // 'Content-Type': 'application/json',
+                  // 'headers': 'Content-Type: application/json',
+                   headers: {
+                      'Content-Type': 'application/json'
+                  },
 
-    //           };
+              };
 
-    //           apigClient.graphqlPost({}, {"query": "{hello}"}, {})
-    //             .then(response => console.log(response))
-    //             .catch(err => console.log(err))
-
-
-    //       });
-
-    //     } else {
-    //       console.log('There was a problem logging you in.');
-    //     }
+              apigClient.graphqlPost({}, {"query": "{hello}"}, {})
+                .then(response => console.log(response))
+                .catch(err => console.log(err))
 
 
-    // });
+          });
+
+        } else {
+          console.log('There was a problem logging you in.');
+        }
+
+
+    });
   }
 
   logout() {
-    // FB.logout(response => {console.log(response)}); 
+    FB.logout(response => {console.log(response)}); 
   }
 }
 
